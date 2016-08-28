7c1e2a3c33a397d051984d86e84e6bdd096ef10a
Roshan <rkhudoyberdiev@gmail.com>
2016-08-28 03:26:15 -0400

    Updated line 67 with conditional

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -64,7 +64,11 @@ get_header( 'shop' ); ?>
 
 			<?php woocommerce_product_loop_start(); ?>
 
-				<?php woocommerce_product_subcategories(); ?>
+				<?php $vendor_shop = urldecode( get_query_var( 'vendor_shop' ) );
+				$vendor_id   = WCV_Vendors::get_vendor_id( $vendor_shop );
+				if ( !$vendor_id ) {
+    					woocommerce_product_subcategories();
+				} ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
